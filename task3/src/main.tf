3aa328c4bd2d97bc1919e113c6f4b020f103eaa5
akamsoft <alexkam75@yandex.ru>
2026-02-19 02:47:01 +0300

    Update main.tf

--- task3/src/main.tf
+++ task3/src/main.tf
@@ -22,8 +22,7 @@ module "vpc_dev" {
   source = "./modules/vpc"
   name     = "dev"
   subnets = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-    { zone = "ru-central1-b", cidr = "10.0.2.0/24" }
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" }
   ]
 }
 
