fc7630e56a255c5f6ec48c94d277aa9b270cefd9
akamsoft <alexkam75@yandex.ru>
2026-02-19 01:48:53 +0300

    Add files via upload

--- task2/src/main.tf
+++ task2/src/main.tf
@@ -55,8 +55,8 @@ module "example-vm" {
   source         = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name       = "dev"
   network_id     = module.vpc_dev.vpc_id
-  subnet_zones   = [module.vpc_dev.subnet_zones[1]]
-  subnet_ids     = [module.vpc_dev.subnet_ids[1]]
+  subnet_zones   = [module.vpc_dev.subnet_zones[0]]
+  subnet_ids     = [module.vpc_dev.subnet_ids[0]]
   instance_name  = "web2"
   instance_count = 1
   image_family   = "ubuntu-2004-lts"
